15c1c5244230706959c002a449c6c900ef51cc86
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-08-01 18:57:27 +0100

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,1 +1,20 @@
-qwertyu
+#!/usr/bin/env python3
+"""
+Flask app
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Handles / route
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
