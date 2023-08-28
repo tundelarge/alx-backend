21ee246535ed3b7590421febc244adfdddd5d27a
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-08-28 20:31:08 +0100

    Update 6-job_processor.js

--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -1,1 +1,12 @@
-qwertyu
+import { createQueue } from 'kue';
+
+const queue = createQueue();
+
+function sendNotification(phoneNumber, message) {
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+};
+
+queue.process('push_notification_code', function(job, done) {
+  sendNotification(job.data.phoneNumber, job.data.message);
+  done();
+});
