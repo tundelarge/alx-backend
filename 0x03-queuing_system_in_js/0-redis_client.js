6fef68559536415823718d49481b4d8e8fbcc743
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-08-28 19:50:49 +0100

    Update 0-redis_client.js

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,1 +1,14 @@
-qwertyu
+import { createClient } from 'redis';
+
+function redisConnect() {
+  const client = createClient();
+
+  client.on('connect', function() {
+    console.log('Redis client connected to the server');
+  }).on('error', (err) => {
+    console.log(`Redis client not connected to the server: ${err}`);
+  });
+
+};
+
+redisConnect();
