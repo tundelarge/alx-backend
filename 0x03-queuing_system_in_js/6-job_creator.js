73881a3527787d5a9e6f37d6e934742a46ccc893
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-08-28 20:30:08 +0100

    Update 6-job_creator.js

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,1 +1,20 @@
-qwertyu
+import { createQueue } from 'kue';
+
+const queue = createQueue();
+
+const notification = {
+  'phoneNumber': '4153518780',
+  'message': 'This is the code to verify your account'
+}
+
+const job = queue.create('push_notification_code', notification).save(function (error) {
+  if (!error) {
+    console.log(`Notification job created: ${job.id}`);
+  }
+});
+
+job.on('complete', function() {
+    console.log('Notification job completed');
+}).on('failed', function() {
+    console.log('Notification job failed');
+});
